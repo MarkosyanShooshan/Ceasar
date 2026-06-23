e535f2ef3dab89907ae69449750f123967ff00e1
MarkosyanShooshan <markosyanshooshan@gmail.com>
2026-06-23 14:09:42 +0400

    Refactor Caesar cipher implementation

--- game_cezar_function.py
+++ game_cezar_function.py
@@ -1,28 +1,29 @@
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 def caesar(start_text, shift_amount, cipher_direction):
-    end_text = " "
-    if cipher_direction=="decode":
+    end_text = "" 
+    if cipher_direction == "decode":
         shift_amount *= -1
     for i in start_text:
         if i in alphabet:
             position = alphabet.index(i)
             new_position = position + shift_amount
-            end_text+= alphabet[new_position]
+            end_text += alphabet[new_position]
         else:
             end_text += i
     print(f"Here's the {cipher_direction} result: {end_text}")  
+
 should_end = False 
 while not should_end:
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n") 
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower().strip() 
     text = input("Type your message:\n").lower() 
     shift = int(input("Type the shift number:\n"))
 
-    shift = shift %26  
+    shift = shift % 26  
     caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
-    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n'") 
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower().strip() 
     if restart == "no":
         should_end = True
-        print("Goodbye")           
+print("Goodbye!")
 
 
